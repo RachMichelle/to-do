f8587604c0d7f757a012b9cc5746bf044560f6ad
Rachel <rachel@LAPTOP-2HAP8AE4.localdomain>
2024-04-02 09:01:40 -0400

    added complete section

--- app.js
+++ app.js
@@ -4,18 +4,21 @@ let completeList = document.querySelector('.done');
 const taskInput = document.querySelector('#addTodo');
 let storedTodos = [];
 let storedComplete = [];
-const box = document.querySelectorAll("input[type='checkbox']")
 
 let getTodos = (JSON.parse(localStorage.getItem("storedList")));
-// let getDones = (JSON.parse(localStorage.getItem('storedDones')));
+let getDones = (JSON.parse(localStorage.getItem("storedDones")));
 
-for (let retrieved of getTodos){
+if (getTodos !== null){
+    for (let retrieved of getTodos){
     storedTodos.push(retrieved);
+    }
 }
 
-// for (let done of getDones){
-//     storedComplete.push(done);
-// }
+if (getDones !== null){
+    for (let done of getDones){
+    storedComplete.push(done);
+}
+}
 
 for (let storedTodo of storedTodos){
     let storedTask = document.createElement("li");
@@ -23,20 +26,35 @@ for (let storedTodo of storedTodos){
     todoList.appendChild(storedTask);
 }
 
-// for (let storedDone of storedComplete){
-//     let storedDoneTask = document.createElement("li");
-//     storedDoneTask.innerHTML = storedDone;
-//     completeList.appendChild(storedDoneTask);
-// }
+for (let storedDone of storedComplete){
+    let storedDoneTask = document.createElement("li");
+    storedDoneTask.innerHTML = storedDone;
+    completeList.appendChild(storedDoneTask);
+    }
+
+    for (box of document.querySelectorAll(".checked")){
+        box.checked=true;
+    }    
 
 todoList.addEventListener("click", function(e){
    if (e.target.checked === true){
+    for (let item of storedTodos){
+        if (storedTodos.includes(e.target.parentElement.innerHTML)){
+            storedTodos.splice(storedTodos.indexOf[item], 1);
+            localStorage.clear();
+            localStorage.setItem("storedList", JSON.stringify(storedTodos));
+        }
+    }
     newDone = document.createElement('li');
     newDone.innerHTML = e.target.parentElement.innerHTML;
     newDone.classList.add("complete");
     e.target.parentElement.remove();
     completeList.appendChild(newDone);
     document.querySelector('.complete > input[type="checkbox"]').checked=true;
+    document.querySelector('.complete > input[type="checkbox"]').classList.add("checked");
+    storedComplete.push(newDone.innerHTML);
+    localStorage.setItem("storedDones", JSON.stringify(storedComplete));
+    //old code:
     // e.target.parentElement.className = "clicked";
     // e.target.classList.add("checked")
    }
@@ -55,6 +73,7 @@ todoList.addEventListener("click", function(e){
             storedTodos.splice(storedTodos.indexOf[todo], 1)
             localStorage.clear();
             localStorage.setItem("storedList", JSON.stringify(storedTodos));
+            localStorage.setItem("storedDones", JSON.stringify(storedComplete));
             }
         }
     }   
@@ -63,17 +82,34 @@ todoList.addEventListener("click", function(e){
 completeList.addEventListener("click", function(e){
     if (e.target.tagName === "BUTTON"){
         e.target.parentElement.remove();
+        for (let deleted of storedComplete){
+            if (storedComplete.includes(e.target.parentElement.innerHTML)){
+                storedComplete.splice(storedComplete.indexOf[deleted], 1);
+                localStorage.clear();
+                localStorage.setItem("storedDones", JSON.stringify(storedComplete));
+                localStorage.setItem("storedList", JSON.stringify(storedTodos));
+            }
+        }
     }
 })
 
 completeList.addEventListener("click", function(e){
     if (e.target.checked === false){
+        for (let undo of storedComplete){
+            if (storedComplete.includes(e.target.parentElement.innerHTML)){
+                storedComplete.splice(storedComplete.indexOf[undo], 1);
+                localStorage.clear();
+                localStorage.setItem("storedDones", JSON.stringify(storedComplete));
+            }
+        }
         undoDone = document.createElement('li');
         undoDone.innerHTML = e.target.parentElement.innerHTML;
         undoDone.classList.remove("complete");
+        undoDone.firstElementChild.classList.remove("checked");
         e.target.parentElement.remove();
         todoList.appendChild(undoDone);
-        // document.querySelector('.todoList > input[type="checkbox"]').checked=false;
+        storedTodos.push(undoDone.innerHTML);
+        localStorage.setItem("storedList", JSON.stringify(storedTodos));
     }
 })
 
